f094bc689eb46212883d8336b3324370ec75de36
Vinayak Mukherjee <31313708+vp93@users.noreply.github.com>
2018-11-02 01:20:54 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/Vinnn/pen/zmbpBa.
+
+ Animated Concert Tickets with a whole bunch of information. 
+Some on reveal and some on first glance, based on order of importance. 
+
---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('.toggle-tickets').click(function() {
+  $tickets = $(this).parent().siblings('.collapse');
+ 
+  if ($tickets.hasClass('in')) {
+    $tickets.collapse('hide');
+    $(this).html('Show Tickets');
+    $(this).closest('.ticket-card').removeClass('active');
+  } else {
+    $tickets.collapse('show');
+    $(this).html('Hide Tickets');
+    $(this).closest('.ticket-card').addClass('active');
+  }
+});
